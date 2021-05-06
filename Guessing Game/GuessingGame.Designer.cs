87800075d4f1dbb64f7b397eb5875eb0264914b6
Kyle Jantzi <kylejantzi@outlook.com>
2021-05-06 14:04:37 -0400

    Add project files.

---
+++ Guessing Game/GuessingGame.Designer.cs
@@ -0,0 +1,119 @@
+﻿
+namespace Guessing_Game
+{
+    partial class GuessingGame
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.title = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.outputLabel = new System.Windows.Forms.Label();
+            this.numberInput = new System.Windows.Forms.TextBox();
+            this.guessButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // title
+            // 
+            this.title.Font = new System.Drawing.Font("Dutch801 XBd BT", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.title.ForeColor = System.Drawing.Color.White;
+            this.title.Location = new System.Drawing.Point(1, 25);
+            this.title.Name = "title";
+            this.title.Size = new System.Drawing.Size(490, 37);
+            this.title.TabIndex = 0;
+            this.title.Text = "Guessing Game";
+            this.title.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("High Tower Text", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(1, 146);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(490, 37);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Enter a Number Between 1 and 100";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // outputLabel
+            // 
+            this.outputLabel.Font = new System.Drawing.Font("High Tower Text", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.outputLabel.ForeColor = System.Drawing.Color.White;
+            this.outputLabel.Location = new System.Drawing.Point(1, 420);
+            this.outputLabel.Name = "outputLabel";
+            this.outputLabel.Size = new System.Drawing.Size(490, 37);
+            this.outputLabel.TabIndex = 2;
+            this.outputLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // numberInput
+            // 
+            this.numberInput.Font = new System.Drawing.Font("High Tower Text", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numberInput.Location = new System.Drawing.Point(195, 232);
+            this.numberInput.Name = "numberInput";
+            this.numberInput.Size = new System.Drawing.Size(100, 39);
+            this.numberInput.TabIndex = 3;
+            this.numberInput.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // guessButton
+            // 
+            this.guessButton.Font = new System.Drawing.Font("High Tower Text", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.guessButton.Location = new System.Drawing.Point(176, 296);
+            this.guessButton.Name = "guessButton";
+            this.guessButton.Size = new System.Drawing.Size(138, 48);
+            this.guessButton.TabIndex = 4;
+            this.guessButton.Text = "Guess";
+            this.guessButton.UseVisualStyleBackColor = true;
+            this.guessButton.Click += new System.EventHandler(this.guessButton_Click);
+            // 
+            // GuessingGame
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(489, 466);
+            this.Controls.Add(this.guessButton);
+            this.Controls.Add(this.numberInput);
+            this.Controls.Add(this.outputLabel);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.title);
+            this.Name = "GuessingGame";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Guessing Game";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label title;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label outputLabel;
+        private System.Windows.Forms.TextBox numberInput;
+        private System.Windows.Forms.Button guessButton;
+    }
+}
+
---
+++ Guessing Game/GuessingGame.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Guessing_Game
+{
+    public partial class GuessingGame : Form
+    {
+        //Global variables
+        int randomNumber;
+        Random randGen = new Random();
+
+        private void guessButton_Click(object sender, EventArgs e)
+        {
+            
+            try
+            {
+                int userGuess;
+                userGuess = Convert.ToInt32(numberInput.Text);
+                
+                if (userGuess < 0)
+                {
+                    outputLabel.Text = "You must enter a positive number";
+                    numberInput.Text = "";
+                }
+                else if (userGuess < randomNumber)
+                {
+                    outputLabel.Text = "Too Low";
+                    numberInput.Text = "";
+                }
+                else if (userGuess > randomNumber)
+                {
+                    outputLabel.Text = "Too High";
+                    numberInput.Text = "";
+                }
+                else
+                {
+                    outputLabel.Text = "You Got it!";
+                }
+            }
+            catch
+            {
+                outputLabel.Text = "You must enter a whole number";
+                numberInput.Text = "";
+            }
+            
+        }
+
+        public GuessingGame()
+        {
+            InitializeComponent();
+            randomNumber = randGen.Next(1, 101);
+        }
+    }
+}
